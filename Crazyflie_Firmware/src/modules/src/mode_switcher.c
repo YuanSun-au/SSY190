cb1b8dcc05c490e76112fdeee5e751a18662c353
Marcus_ <marcus@marcusolsson.org>
2016-05-12 14:10:05 +0200

    both queues

--- Crazyflie_Firmware/src/modules/src/mode_switcher.c
+++ Crazyflie_Firmware/src/modules/src/mode_switcher.c
@@ -12,7 +12,8 @@ or STOP (controller off)
 static bool isInit;
 static int status=0;
 int FREQ;
-static QueueHandle_t* xQueue1,xQueue2;
+static QueueHandle_t* xQueue1;
+static QueueHandle_t* xQueue2;
 
 static int toggle(int var){
   return var?0:1;
