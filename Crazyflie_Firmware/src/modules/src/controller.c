737d33093cc512b7d78deb5e9e9d5bd39f6802d3
Marcus_ <marcus@marcusolsson.org>
2016-05-13 11:16:14 +0200

    time to wait

--- Crazyflie_Firmware/src/modules/src/controller.c
+++ Crazyflie_Firmware/src/modules/src/controller.c
@@ -40,7 +40,7 @@ static void controllerTask(void* param)
   while(1)
   {
     //vTaskDelayUntil(&lastWakeTime, F2T(*FREQ)); // delay until new ref or state estimation
-    if(xQueueReceive( xQueue1, &( FREQ ),portMAX_DELAY ))
+    if(xQueueReceive( xQueue1, &( FREQ ),( TickType_t ) 10 ))
       { // if/else needed?
     // Get error
     //e=ref-state;
