3421925b51ace9062e38ef160a339774f8cd9474
Marcus_ <marcus@marcusolsson.org>
2016-05-19 11:55:18 +0200

    new inv table

--- Matlab/ControllerDesign.m
+++ Matlab/ControllerDesign.m
@@ -26,10 +26,13 @@ Iz = 2.173e-5;
 % R2;   % Rotation used to transform rpy to w
 %%
 syms d k b;
- TorqueThrust=[-b, -b, -b, -b;
-                   0, -d*b, 0, d*b;
-                   d*b, 0, -d*b, 0;
-                   k, -k, k, -k]; % generates Thrust(sum) and torque(1,2,3)
+b=2.75e-11;% lift constant
+k=1e-9;% drag constant
+
+ TorqueThrust=[-1, -1, -1, -1;
+                   0, -d, 0, d;
+                   d, 0, -d, 0;
+                   k/b, -k/b, k/b, -k/b]; % generates Thrust(sum) and torque(1,2,3)
  Rotz=[ cosd(45) -sind(45) 0;
         sind(45) cosd(45) 0;
         0   0   1];
@@ -57,13 +60,12 @@ sysd=c2d(sys,Ts);
 
 %% Controller design
 % Select parameters
-Q=diag([1e2, 1e2, 1,... % r,p,y
+Q=diag([1e1, 1e1, 1,... % r,p,y
     1e1, 1e1, 1,... % p,q,r
     1, 1]); %dz z
 R=diag(100*[0.1, 0.1, 0.1, 0.1]); % thrust,Tx,Ty,Tz
-[K,S,e] = lqr(sysd,Q,R); % K is the feedback vector
-e
-%closed_poles=eig(sysd.a-sysd.b*K)
+K = lqr(sysd,Q,R) % K is the feedback vector
+closed_poles=eig(A-B*K)
 %% Kalman design
 % A=zeros(12);
 % A(1:3,4:6)=eye(3);
@@ -113,9 +115,9 @@ fclose(fb);
 K_c=fopen('feedback_c.txt','w');
 for n=1:length(K(:,1))
     fprintf(K_c,'{');
-    fprintf(K_c,'%10.15f',K(n,1));
+    fprintf(K_c,'%10.10f',K(n,1));
     for i=K(n,2:end)
-        fprintf(K_c,',%10.15f',i);
+        fprintf(K_c,',%10.10f',i);
     end
     fprintf(K_c,'},\n');
 end
