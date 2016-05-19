0231e531c2d2e10bec51058051b4fd11279062a9
Marcus_ <marcus@marcusolsson.org>
2016-05-19 14:52:20 +0200

    New weight on yaw speed

--- Matlab/ControllerDesign.m
+++ Matlab/ControllerDesign.m
@@ -58,7 +58,7 @@ sysd=c2d(sys,Ts);
 %% Controller design
 % Select parameters
 Q=diag([1e-2, 1e-2, 1e-1,... % r,p,y
-    1e-1, 1e-1, 1e-1,... % p,q,r
+    1e-1, 1e-1, 1e0,... % p,q,r
     1e0, 1e0]); %dz z
 R=diag(10*[1, 1, 1, 1]); % thrust,Tx,Ty,Tz
 K = lqr(sysd,Q,R) % K is the feedback vector
