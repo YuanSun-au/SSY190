4a12c1560d20a1af3ebc09a08c8a367f93c39596
welzchalmers <romanwelz@Romans-MBP.lan>
2016-05-22 13:11:17 +0200

    disabled commander
    min thrust value seemed to be off, trying to get the real one

--- Crazyflie_Firmware/src/modules/src/controller.c
+++ Crazyflie_Firmware/src/modules/src/controller.c
@@ -198,7 +198,7 @@ static void controllerTask(void* param)
         x[2] -=360.0;
       else if (x[2] < -180.0)
         x[2] +=360.0;
-      baseThrust = ref_generatorExtIn(ref);
+      //baseThrust = ref_generatorExtIn(ref);   // DEBUG --- DISABLES THE COMMANDER
       // Calculate input (T,tx,ty,tz)
       float xxi[2*Nstates] = {x[0],x[1],x[2],x[3],x[4],x[5],xi[0],xi[1],xi[2],xi[3],xi[4],xi[5]};
 
