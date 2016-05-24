bc001df17566fb8c6ccb2bc11cbb5db22bfeac6f
welzchalmers <romanwelz@dhcp-178104.eduroam.chalmers.se>
2016-05-24 08:56:33 +0200

    clean up

--- Crazyflie_Firmware/src/modules/src/ref_generator.c
+++ Crazyflie_Firmware/src/modules/src/ref_generator.c
@@ -14,29 +14,14 @@ OUT: reference to controller
 #include "queue.h"
 // From Crazyflie
 #include "commander.h"
-#include "led.h"
 #include "config.h"
 #include "system.h"
 #include "ref_generator.h"
-#include "log.h"
 #include "debug.h"
-//#include "param.h"
-
 // multiplicator on the received thrust
 #define THRUST_M 0.808715820312500
 
 static bool isInit;
-//extern QueueHandle_t xQueue2;
-int* FREQ;
-
-// euler angles from sensorFusion
-//static float eulerRollActual;
-//static float eulerPitchActual;
-//static float eulerYawActual;
-// euler angles from user
-//static float eulerRollDesired;
-//static float eulerPitchDesired;
-//static float eulerYawDesired;
 
 static void ref_generatorTask(void* param)
 {
@@ -49,54 +34,12 @@ static void ref_generatorTask(void* param)
 
   lastWakeTime = xTaskGetTickCount ();
 
-  //int ledstatus = 0; // for the ledToggle
-
   while(1)
   {
     vTaskDelayUntil(&lastWakeTime, F2T(10)); // delay until next
-  //  xQueueReceive( xQueue2, &( FREQ ), portMAX_DELAY ); // if/else needed?
-    // Get reference (from where?) (from commander)
-    //commanderGetRPY(&eulerRollDesired, &eulerPitchDesired, &eulerYawDesired); // sets the desired to what the user want
-
-    // cretate a smooth trajectory for the reference (maybe a derivative?)
-
-    // Tell controller we have a new reference (if we have? do we need to?)
-
-    // For this week we just toggle some leds
-    //DEBUG_PRINT("ref_gen got message: %d\n",*FREQ);
-    //ledSet(CHG_LED,ledstatus);
-    //ledstatus = toggle(ledstatus);
 
   }
 }
-
-// static float ref_generatorSetAngle (float value)
-// {
-// // returns an angle of +1 degree or -1 degree, if the commander input is positive, respectively negative
-//   if (value < -10) {
-//     return -ANGLE;
-//   }
-//   else if (value > 10) {
-//     return ANGLE;
-//   }
-//   else {
-//     return 0;
-//   }
-// }
-
-// static float ref_generatorSetZ (uint16_t thrust)
-// {
-// // returns a speed in Z of +0.05 or -0.05 if the commander input is positive, repectively negative
-//   if (thrust < -5) {
-//     return -Z_SPEED;
-//   }
-//   else if (thrust > 5) {
-//     return Z_SPEED;
-//   }
-//   else {
-//     return 0;
-//   }
-// }
 
 
 static float ref_generatorSetZThrust (uint16_t thrust)
@@ -120,14 +63,13 @@ void ref_generatorInit(void)
   xTaskCreate(ref_generatorTask, REF_GEN_TASK_NAME,
               REF_GEN_TASK_STACKSIZE, NULL, REF_GEN_TASK_PRI, NULL);
 
-
   isInit = true;
 }
+
 bool ref_generatorTest(void)
 {
   return true;
 }
-
 
 float ref_generatorExtIn (float* xRef)
 {
@@ -140,11 +82,5 @@ float ref_generatorExtIn (float* xRef)
   xRef[0]=rollRef*1.3;  //ref_generatorSetAngle(rollRef);
   xRef[1]=pitchRef*1.3; //ref_generatorSetAngle(pitchRef);
 
-
   return ref_generatorSetZThrust(thrust);
 }
-
-/* Loggable variables */
-LOG_GROUP_START(ref)
-LOG_ADD(LOG_INT8, ref_freq, &FREQ)
-LOG_GROUP_STOP(ref)
